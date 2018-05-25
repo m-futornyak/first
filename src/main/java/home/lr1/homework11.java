077318aed86ff51f2758a2be2ce6f6056f189841
mfutornyak <mfutornyak@playtika.com>
2018-05-25 20:29:04 +0300

    first commit

--- src/main/java/home/lr1/homework11.java
+++ src/main/java/home/lr1/homework11.java
@@ -4,12 +4,14 @@ package home.lr1;
  * Created by mfutornyak on 25.05.2018.
  */
 public class homework11 {
-    public static double main (long) {
+    public static void main(String[] args) {
+
         long x = 100;
         long y = 20;
         long z = 2;
         long n = 3;
         long rezult1 = x - (y + 123) / z * n;
         System.out.printf("Результат уравнения = " + rezult1);
+
     }
 }
