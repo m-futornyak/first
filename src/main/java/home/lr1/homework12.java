562e99cc14267bd3a19b4761c59d1ff3d65f5600
mfutornyak <mfutornyak@playtika.com>
2018-05-25 23:46:58 +0300

    first commit

--- src/main/java/home/lr1/homework12.java
+++ src/main/java/home/lr1/homework12.java
@@ -5,17 +5,23 @@ package home.lr1;
  */
 public class homework12 {
     public static void main(String[] args) {
-        int a = 45678;
-        int r1 = a%45674;
-            System.out.printf("первaя цифра числа = " + r1);
-        int r2 = a%45673;
-            System.out.printf("%nвторая цифра числа = " + r2);
-        int r3 = a%45672;
-            System.out.printf("%nтретья цифра числа = " + r3);
-        int r4 = a%45671;
-            System.out.printf("%nчетвертая цифра числа = " + r4);
-        int r5 = a%45670;
-            System.out.printf("%nпятая цифра числа = " + r5);
+        int a = 81678;
+       int r1 = a/10000;
+            System.out.printf("первaя цифра числа " + a +  " = " + r1);
+        int r21 = a/1000;
+        int r2 =  r21%10;
+            System.out.printf("%nвторая цифра числа " + a +  " = " + r2);
+       int r31 = a/100;
+        int r3 = r31%10;
+            System.out.printf("%nтретья цифра числа " + a + " = " + r3);
+        int r41 = a%100;
+         int r4 = r41 / 10;
+            System.out.printf("%nчетвертая цифра числа " + a + " = " + r4);
+        int r51 = a%100;
+        int r5 = r51%10;
+            System.out.printf("%nпятая цифра числа " + a + " = " + r5);
+
+
 
     }
 }
