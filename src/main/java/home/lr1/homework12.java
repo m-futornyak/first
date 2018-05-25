d7d9af70929d3e3e08e19026e372e3088eca6511
mfutornyak <mfutornyak@playtika.com>
2018-05-25 21:53:48 +0300

    first commit

--- src/main/java/home/lr1/homework12.java
+++ src/main/java/home/lr1/homework12.java
@@ -5,6 +5,17 @@ package home.lr1;
  */
 public class homework12 {
     public static void main(String[] args) {
+        int a = 45678;
+        int r1 = a%45674;
+            System.out.printf("первaя цифра числа = " + r1);
+        int r2 = a%45673;
+            System.out.printf("%nвторая цифра числа = " + r2);
+        int r3 = a%45672;
+            System.out.printf("%nтретья цифра числа = " + r3);
+        int r4 = a%45671;
+            System.out.printf("%nчетвертая цифра числа = " + r4);
+        int r5 = a%45670;
+            System.out.printf("%nпятая цифра числа = " + r5);
 
     }
 }
