75838fad18eb584c5a3b1a0498701d9c0bc9e349
mfutornyak <mfutornyak@playtika.com>
2018-05-25 21:15:07 +0300

    first commit

--- src/main/java/home/lr1/homework11.java
+++ src/main/java/home/lr1/homework11.java
@@ -11,7 +11,14 @@ public class homework11 {
         long z = 2;
         long n = 3;
         long rezult1 = x - (y + 123) / z * n;
-        System.out.printf("Результат уравнения = " + rezult1);
+            System.out.printf("Результат уравнения 1 = " + rezult1);
+        int rezult2 = (int) ((x + (50 - y * z) / n) - x);
+            System.out.printf("%nРезультат уравнения 2 = " + rezult2);
+        float rezult3 = x / (y * (z - 1) - 564) + n;
+            System.out.printf("%nРезультат уравнения 3 = " + rezult3);
+        double rezult4 = -x / -y + z / (n + 15);
+            System.out.printf("%nРезультат уравнения 4 = " + rezult4);
 
     }
+
 }
---
+++ src/main/java/home/lr1/homework12.java
@@ -0,0 +1,10 @@
+package home.lr1;
+
+/**
+ * Created by mfutornyak on 25.05.2018.
+ */
+public class homework12 {
+    public static void main(String[] args) {
+
+    }
+}
