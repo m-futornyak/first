bed8b51236289d6817435a8fdc3831e544b64f30
mfutornyak <mfutornyak@playtika.com>
2018-05-25 17:24:01 +0300

    first commit

---
+++ src/main/java/home/lr1/HelloWorld.java
@@ -0,0 +1,16 @@
+package home.lr1;
+
+/**
+ * Created by mfutornyak on 25.05.2018.
+ */
+public class HelloWorld {
+public static void main(String[] args) {
+    System.out.println("hello world!");
+
+
+}
+
+}
+
+
+
